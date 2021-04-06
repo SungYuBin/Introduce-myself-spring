6a6eac84c75c01502b7f33f9d3a11e61aacb755f
SUNG YU BIN <31871417+SungYuBin@users.noreply.github.com>
2021-04-06 13:43:46 +0900

    Initial commit

---
+++ src/main/java/Introducemyself/Introducemyselfspring/IntroduceMyselfSpringApplication.java
@@ -0,0 +1,13 @@
+package Introducemyself.Introducemyselfspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroduceMyselfSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroduceMyselfSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Introducemyself/Introducemyselfspring/IntroduceMyselfSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package Introducemyself.Introducemyselfspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroduceMyselfSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
