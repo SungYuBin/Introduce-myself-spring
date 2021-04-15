e7c0b1874703d1b3c15cfa9b8935ca202c465a30
SUNG YU BIN <31871417+SungYuBin@users.noreply.github.com>
2021-04-15 17:37:42 +0900

    로그인기능 구현중

--- src/main/java/Introducemyself/Introducemyselfspring/controller/LoginController.java
+++ src/main/java/Introducemyself/Introducemyselfspring/controller/LoginController.java
@@ -3,15 +3,16 @@ package Introducemyself.Introducemyselfspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("login")
-     public String login()
+    @GetMapping("join")
+     public String login(Model model)
     {
-
-        return "login";
+        model.addAttribute("msg", "메시지");
+        return "join";
     }
 
 }
