8af72247e7910ba408b0c5a3cc4746c68008a4ab
SUNG YU BIN <31871417+SungYuBin@users.noreply.github.com>
2021-04-16 15:55:39 +0900

    메인화면 구성

--- src/main/java/Introducemyself/Introducemyselfspring/controller/LoginController.java
+++ src/main/java/Introducemyself/Introducemyselfspring/controller/LoginController.java
@@ -9,10 +9,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class LoginController {
 
     @GetMapping("join")
-     public String login(Model model)
+     public String Getjoin(Model model)
     {
         model.addAttribute("msg", "메시지");
         return "join";
+    }
+
+    @PostMapping("join")
+    public String Postjoin(UserInfo userInfo) //값이 정상적으로 넘어옵니다
+    {
+        int i=0;
+        return "redirect:/";
+
+    }
+
+    @PostMapping("login")
+    public String Postlogin(Model model){
+        model.addAttribute("","");
+        return "main";
     }
 
 }
